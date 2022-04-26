f79ae56554f0232c368da714041a8e998dfe30fc
Madison Law <madisonlaw1@gmail.com>
2022-04-26 09:47:36 -0600

    Add POST fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,12 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+
+export const postUrls = (newUrl) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: "POST",
+    body: JSON.stringify(newUrl),
+    headers: {"Content-Type": "application/json"}
+  }).then(response => response.json())
+}
