b362da0ef0637d60b7be59c33bc1a03a9c4e0fd4
Madison Law <madisonlaw1@gmail.com>
2022-04-26 10:34:12 -0600

    Add first test for page title

--- cypress/integration/main_dashboard_spec.js
+++ cypress/integration/main_dashboard_spec.js
@@ -28,4 +28,17 @@ const postShortenedUrls = () => {
   }
 }
 
+describe('Main dashboard user flow', () => {
 
+  it('should display the page title and existing shortened URLs', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', getShortenedUrls()).as('getUrls')
+    cy.visit('http://localhost:3000/')
+    .get('.page-title').contains('URL Shortener')
+    .get('.url').first().find('.url-title').contains('Awesome photo')
+    .get('.url').first().find('.short-url').contains('http://localhost:3001/useshorturl/1')
+    .get('.url').first().find('.long-url').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+  });
+
+
+
+})
