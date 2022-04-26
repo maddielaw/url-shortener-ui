18ba38393dbc8f632efe760005634c84953089c2
Madison Law <madisonlaw1@gmail.com>
2022-04-26 09:32:34 -0600

    Add fetch call to componentDidMount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,12 @@ export class App extends Component {
   }
 
   componentDidMount() {
+    getUrls()
+      .then((data) => {
+        this.setState({
+          urls: data.urls
+        })
+      })
   }
 
   render() {
